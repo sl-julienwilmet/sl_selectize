8ef6882d8742cdec42d85a2eefe7473f833b69ce
Kevin Rostagni <kevin.rostagni@seloger.com>
2019-04-24 14:38:29 +0200

    prevent $order of undefiend optgroup

--- src/selectize.js
+++ src/selectize.js
@@ -1100,8 +1100,8 @@ $.extend(Selectize.prototype, {
 		// sort optgroups
 		if (this.settings.lockOptgroupOrder) {
 			groups_order.sort(function(a, b) {
-				var a_order = self.optgroups[a].$order || 0;
-				var b_order = self.optgroups[b].$order || 0;
+				var a_order = self.optgroups[a] && self.optgroups[a].$order || 0;
+				var b_order = self.optgroups[b] && self.optgroups[b].$order || 0;
 				return a_order - b_order;
 			});
 		}
